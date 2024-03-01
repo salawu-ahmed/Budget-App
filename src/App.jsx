945d7ba5b21244d890540b8dfd604e4dc293e809
ahmed <a.salawu@outlook.com>
2024-03-01 16:13:38 +0000

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='container'>
+      <h1 className="mt-3">My Budget Planner</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
